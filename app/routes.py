ca34f265a84d9db3909fc27ae24fe92791d53d8a
milkmilkmilkmilkmilk <131097756+milkmilkmilkmilkmilk@users.noreply.github.com>
2025-09-09 17:59:01 +0900

    Fix HTML comments

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,8 @@ from flask import Blueprint, render_template, request, redirect, url_for
 # ここでBlueprintを作成している。mainはBlueprintの名。__name__はこのファイルの場所を教える為の値
 bp = Blueprint('main', __name__)
 
-# 
-posts = [] # 投稿データを格納するリスト。実際のアプリケーションではデータベースを使用することが一般的です。
+# 投稿データを格納するリスト。実際のアプリケーションではデータベースを使用することが一般的です。
+posts_data = []
 
 # 投稿一覧
 @bp.route('/')
@@ -19,18 +19,15 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-        posts.append({'title':title, 'content': content})
-        print('新しい投稿: {title} - {content}') # 投稿内容をサーバー側で確認するためにターミナルに表示します。デバッグ目的です。
-        return redirect(url_for('main.index')) # 投稿が完了したら(/)にリダイレクトしている。url_for('main.index')でindex()関数のURLを自動で取得。
+        posts_data.append({'title': title, 'content': content})
+        # 投稿内容をサーバー側で確認するためにターミナルに表示（デバッグ目的）
+        print(f'新しい投稿: {title} - {content}')
+        # 投稿が完了したらトップページにリダイレクト
+        return redirect(url_for('main.index'))
     return render_template('form.html')    
 
 @bp.route('/posts')
 def posts():
-    dummy_posts = [
-        {'title': 'タイトル壱', 'content': 'Hello World!'},
-        {'title': 'タイトル弐', 'content': 'Flask is great!'},
-        {'title': 'タイトル参', 'content': 'Python is awesome!'}
-    ]
-    # ダミーの投稿データを作成しています。実際のアプリケーションでは、データベースから取得することが一般的です。
-    return render_template('posts.html', posts=dummy_posts)
+    # 保存された投稿データを表示する
+    return render_template('posts.html', posts=posts_data)
 
